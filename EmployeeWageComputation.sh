315d0260f10e478b59e3b05603dfe4738a2ec527
Chetnap19 <patilchetna19@gmail.com>
2020-07-04 20:39:53 +0530

    added Employee

---
+++ EmployeeWageComputation.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -x 
+echo "Welcome to Employee Wage Computation Program"
+
+Partime=1
+Fulltime=2
+totalempHrs=0;
+TotalWorkingDays=0;
+HrsInMonth=100;
+empRatePerHrs=20;
+NumberOfWorkingDays=20;
+
+function getWorkingDays(){
+	case $1 in 
+		$fullTime)
+			empHrs=8;
+			;;
+                $partTime)
+			empHrs=4
+		 	;;
+                      *)
+			 empHrs=0
+			 ;;
+	 esac
+	 echo $empHrs;
+ }
+  
+  function calDailyWage(){
+	  local empHrs=$1
+	  Wage=$(($empHrs*$empRatePerHrs))
+	  echo $Wage
+  }
+
+  while [[ $totalempHrs -lt $HrsInMonth && $TotalWorkingDays -lt $NumberOfWorkingDays ]]
+  do
+	  ((TotalWorkingDays++))
+	  empHrs="$( getWorkingDays $((RANDOM%3)) )"
+	  totalempHrs=$(($totalempHrs+$empHrs ))
+	  empDailyWage["$totalworkingdays"]="$( calDailyWage $empHrs )"
+  done
+
+  totalSalary="$( calDailyWage $totalempHrs )"
+  echo "Daily Wage " ${empDailyWage[@]}
+
+
+
